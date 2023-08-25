0978739d4fbcafbc83ef73e188594789b9c1c32c
Seonguk Choi <chsu7086@gmail.com>
2023-08-25 09:23:32 +0900

    Modifying Paths in Template Project (Linux)

--- Template/source/main.cpp
+++ Template/source/main.cpp
@@ -4,8 +4,8 @@ int main() {
 
     DataHandler* dh = new DataHandler();
 
-    dh->read_feature_vector("/home/chsu/CPP-ML/Template/source/MNIST/train-images-idx3-ubyte");
-    dh->read_feature_label("/home/chsu/CPP-ML/Template/source/MNIST/train-labels-idx1-ubyte");
+    dh->read_feature_vector("../source/MNIST/train-images-idx3-ubyte");
+    dh->read_feature_label("../source/MNIST/train-labels-idx1-ubyte");
     dh->split_data();
     dh->count_classes();
 
